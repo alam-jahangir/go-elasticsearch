6b54e898c0b3b1fd80a3bc1646c6a8ad53ff3f18
Md. Jahangir Alam <jahangir033003@gmail.com>
2021-09-23 21:14:26 +0600

    Update a document & Search Data from Elasticsearch by GO

--- main.go
+++ main.go
@@ -49,9 +49,45 @@ func main() {
 	// }
 	// defer res.Body.Close()
 
-	res, err := es.Index("data_ecommerce", esutil.NewJSONReader(&data), es.Index.WithDocumentID("test_data_p12346"), es.Index.WithRefresh("true"))
+	// Insert a Single Document
+	res, err := es.Index("data_ecommerce-2019-12-09", esutil.NewJSONReader(&data), es.Index.WithDocumentID("test_data_p12346"), es.Index.WithRefresh("true"))
 	if err != nil {
 		log.Fatalf("Error getting response: %s", err)
 	}
+	defer res.Body.Close()
+	log.Println(res)
+
+	// Update a single Document
+	updateData := make(map[string]interface{})
+	doc := make(map[string]interface{})
+	updateData["currency"] = "USD"
+	updateData["customer_last_name"] = "World!"
+	doc["doc"] = updateData
+	res, err = es.Update("data_ecommerce-2019-12-09", "test_data_p12346", esutil.NewJSONReader(&doc), es.Update.WithRefresh("true"))
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+
+	defer res.Body.Close()
+	log.Println(res)
+
+	// Search Query
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"match": map[string]interface{}{
+				"customer_first_name": "Hello",
+			},
+		},
+	}
+
+	res, err = es.Search(
+		es.Search.WithIndex("data_ecommerce-2019-12-09"),
+		es.Search.WithBody(esutil.NewJSONReader(&query)),
+		es.Search.WithPretty(),
+	)
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+
 	log.Println(res)
 }
